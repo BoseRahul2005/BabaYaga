d047fe0e0e9ecd7a930f257d9302db2c7c4a1523
Rahul Bose <rahul.basu2005@gmail.com>
2026-09-22 08:18:07 +0530

    Fix typo in module export statement

--- server/config/github.js
+++ server/config/github.js
@@ -3,4 +3,4 @@ const { Octokit } = require("octokit");
 const octokit = new Octokit({
   auth: e3f9e7e12a0e942405c67a90a620de097b5ddc08ec94ea019e0d8d5be29e98a7,
 });
-module.exports = {octokit};
+module.export = {octokit};
