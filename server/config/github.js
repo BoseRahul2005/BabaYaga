1da75b32018d987b8c78c4e35561881feba905de
Rahul Bose <rahul.basu2005@gmail.com>
2026-09-22 08:15:02 +0530

    Update GitHub auth to use hardcoded token
    Replaced GITHUB_TOKEN with a hardcoded token.

--- server/config/github.js
+++ server/config/github.js
@@ -1,6 +1,6 @@
 const { Octokit } = require("octokit");
 
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
+  auth: e3f9e7e12a0e942405c67a90a620de097b5ddc08ec94ea019e0d8d5be29e98a7,
 });
 module.exports = {octokit};
