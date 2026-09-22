99d8fe59909cd06aec005ecd440697debeceec0e
Rahul Bose <rahul.basu2005@gmail.com>
2026-09-22 08:19:39 +0530

    Update GitHub auth to use environment variable

--- server/config/github.js
+++ server/config/github.js
@@ -1,6 +1,6 @@
 const { Octokit } = require("octokit");
 
 const octokit = new Octokit({
-  auth: e3f9e7e12a0e942405c67a90a620de097b5ddc08ec94ea019e0d8d5be29e98a7,
+  auth: process.env.BREVO_API_KEY,
 });
-module.export = {octokit};
+module.exports = {octokit};
